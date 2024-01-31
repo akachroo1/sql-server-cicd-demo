bc41f3ccb6110bb5fb71d4f751988e352c800ec4
Arjun Goel <arjungoel1995@gmail.com>
2024-01-31 06:05:27 +0000

    fixed sql file code

--- bank/query1.sql
+++ bank/query1.sql
@@ -1,21 +1,9 @@
-Create database bank;
-
-GO
-
-Use bank;
-
-GO
-
-Create table details (
+Create table [bank].[details] (
 customer_id INT,
 customer_uuid INT
 );
 
 GO
 
-INSERT INTO details(customer_id, customer_uuid) VALUES (1, 512);
-INSERT INTO details(customer_id, customer_uuid) VALUES (2, 510);
-INSERT INTO details(customer_id, customer_uuid) VALUES (3, 513);
-INSERT INTO details(customer_id, customer_uuid) VALUES (4, 515);
 
 GO
