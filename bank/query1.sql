21ad9c0d07976b50bdabca5697b6e7e94d211dff
Arjun Goel <arjungoel1995@gmail.com>
2024-01-31 05:59:11 +0000

    fixed sql file code

--- bank/query1.sql
+++ bank/query1.sql
@@ -1,16 +1,21 @@
 Create database bank;
 
+GO
+
 Use bank;
+
+GO
 
 Create table details (
 customer_id INT,
 customer_uuid INT
 );
 
+GO
+
 INSERT INTO details(customer_id, customer_uuid) VALUES (1, 512);
 INSERT INTO details(customer_id, customer_uuid) VALUES (2, 510);
 INSERT INTO details(customer_id, customer_uuid) VALUES (3, 513);
 INSERT INTO details(customer_id, customer_uuid) VALUES (4, 515);
-DROP Table details;
 
-Select * from details;
+GO
