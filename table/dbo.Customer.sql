e26adcb32f59da465fa132285c7ab48bc1a6b992
Arjun Goel <arjungoel1995@gmail.com>
2024-02-07 08:37:28 +0000

    Revamped DB schema

---
+++ table/dbo.Customer.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [Customer]
+(
+    [Name] [nvarchar](50) NOT NULL,
+    [Address] [nvarchar](200) NOT NULL,
+    [CreateDate] [datetime] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+INSERT INTO [Customer] ([Name], [Address], [CreateDate]) VALUES ('Arjun', 'Sonipat', '2024-02-07 00:00:00')
+
+
+GO
